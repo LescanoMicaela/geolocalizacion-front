25a61dd7dc3f09260bcdf294ff652f063cd101e5
mlescano <lescano.micaela@gmail.com>
2021-11-01 17:18:45 +0100

    Estilo opcion seleccionada

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  router: string;
+
+  constructor( public _router: Router) {
+    this.router = _router.url;
+   }
 
   ngOnInit(): void {
   }
