bdaab74e2562cdb8a60bd66c1f738cdf761ea080
mlescano <lescano.micaela@gmail.com>
2021-11-20 18:20:08 +0100

    Cambios de configuración para despliegue

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
-const path = require('path');
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+      return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
 const express = require('express');
 const app = express();
+app.use(requireHTTPS);
 
-// Serve static files
-app.use(express.static(__dirname + '/dist/MY_APP_NAME'));
-
-// Send all requests to index.html
+app.use(express.static('./dist/geolocalizacion-front'));
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/MY_APP_NAME/index.html'));
+  res.sendFile('index.html', {root: 'dist/geolocalizacion-front/'}
+);
 });
 
-// default Heroku port
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 8080);
